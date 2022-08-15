7e0adebfbcb7b55bc5a80c3c5af67593c0c76208
Jayden Grant <jayden.grant@calytrix.com>
2022-08-15 22:24:09 +0800

    init

---
+++ lobby-ranks/version.h
@@ -0,0 +1,11 @@
+#pragma once
+#define VERSION_MAJOR 1
+#define VERSION_MINOR 0
+#define VERSION_PATCH 0
+#define VERSION_BUILD 11
+
+#define stringify(a) stringify_(a)
+#define stringify_(a) #a
+
+namespace jlg { namespace rl { namespace lobby_ranks {
+}; }; }; // END namespace jlg::rl::lobby_ranks
